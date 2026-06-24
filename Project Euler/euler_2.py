50e2f17ba5c6730a67336ae2e31d45e80f5e69a8
adiverma231 <verma231@purdue.edu>
2026-06-23 23:02:02 -0400

    Update euler_2.py

--- Project Euler/euler_2.py
+++ Project Euler/euler_2.py
@@ -2,3 +2,9 @@
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 '''
 
+a, b, sum = 0, 1, 0
+while b < 4000000:
+    sum += b if (b % 2 == 0) else 0
+    a, b = b, a + b
+    
+print(sum)
